9815d5370c6588d3b279f8a5f137f793173ab184
okmanamnotyourdad <141069289+okmanamnotyourdad@users.noreply.github.com>
2023-07-31 22:41:35 +0300

    Update damn.js

--- damn.js
+++ damn.js
@@ -1,1 +1,9 @@
 const 1 = "3"
+const { app, BrowserWindow, dialog } = require('electron').remote;
+const fs = require('fs');
+
+// دالة لفتح ملف وعرضه في مكان كتابة الأكواد
+function openFileInEditor(filePath) {
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+        if (err) {
+            dialog.showErrorBox
